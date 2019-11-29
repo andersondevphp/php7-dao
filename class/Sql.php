615348053d460266e21a996d7d72d8c9d2c7cd2b
Anderson Carvalho <andersoninovaarte@gmail.com>
2019-11-29 08:58:37 -0300

    Métodos de lista

--- class/Sql.php
+++ class/Sql.php
@@ -7,15 +7,15 @@ class Sql extends PDO {
 		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 	}
 	// Associando os parametros ao comando. Outro métodos poderão reutilizar.
-	private function setParams($statment, $parameters = array()) {
+	private function setParams($statement, $parameters = array()) {
 		foreach ($parameters as $key => $value) {
 			// Chamada do método caso seja necessário algum tratamento.
-			$this->setParam($key, $value);
+			$this->setParam($statement, $key, $value);
 		}
 	}
 	// bind de um paramentro somente.
-	private function setParam($statment, $key, $value){
-		$statment->bindParam($key, $value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
 	}
 	// $rawQuery = query bruta a ser tratada.
 	public function query($rawQuery, $params = array()) {
